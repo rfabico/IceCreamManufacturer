399d355c4319930e8c5b10e86619168afa487521
igormagun <32044854+igormagun@users.noreply.github.com>
2020-11-18 14:27:21 -0500

    Set up some Flask basics

--- IceCreamMain.py
+++ IceCreamMain.py
@@ -1,8 +1,12 @@
 import cx_Oracle as cx
-import flask as fl
+from flask import Flask, render_template
 import pandas as pd
 import config
 
+app = Flask(__name__)
+
+
+@app.route('/')
 def main():
     dsn_tns = cx.makedsn(config.ip, config.port, config.sid)
     connection = cx.connect(config.user_name, config.password,dsn_tns)
@@ -11,6 +15,8 @@ def main():
     cursor.execute(sql)
     data = cursor.fetchall()
     print(data)
+
+    return render_template('base.html')
 
 
 if __name__ == '__main__':
