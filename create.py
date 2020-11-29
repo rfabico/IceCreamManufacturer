a8f11ed49cfe8c0e5ac62b29c480a8a183ad73b5
Rylan <36867509+rfabico@users.noreply.github.com>
2020-11-29 12:46:13 -0500

    Add files via upload

--- create.py
+++ create.py
@@ -2,14 +2,14 @@ flavours = "CREATE TABLE flavours ( " \
            "flavour_id INTEGER PRIMARY KEY, " \
            "flavour_name VARCHAR2(30) NOT NULL, " \
            "flavour_description VARCHAR2(255)" \
-           ");"
-flavour_pk = "CREATE SEQUENCE flavour_pk START WITH 1;"
+           ")"
+flavour_pk = "CREATE SEQUENCE flavour_pk START WITH 1"
 ingredients = "CREATE TABLE ingredients ( " \
               "ingredient_id INTEGER PRIMARY KEY, " \
               "ingredient_quantity VARCHAR2(30) NOT NULL, " \
               "ingredient_name VARCHAR2(30) NOT NULL " \
-              ");"
-ingredients_pk = "CREATE SEQUENCE pk_value START WITH 1;"
+              ")"
+ingredients_pk = "CREATE SEQUENCE pk_value START WITH 1"
 suppliers = "CREATE TABLE suppliers ( " \
             "supplier_id INTEGER PRIMARY KEY, " \
             "supplier_name VARCHAR2(30) NOT NULL, " \
@@ -19,8 +19,8 @@ suppliers = "CREATE TABLE suppliers ( " \
             "rep_phone_no VARCHAR2(30) NOT NULL, " \
             "supplier_address VARCHAR2(100) NOT NULL, " \
             "supplier_city VARCHAR2(30) NOT NULL " \
-            ");"
-supplier_pk = "CREATE SEQUENCE supplier_pk START WITH 1;"
+            ")"
+supplier_pk = "CREATE SEQUENCE supplier_pk START WITH 1"
 brands = "CREATE TABLE brands ( " \
          "brand_id INTEGER PRIMARY KEY, " \
          "brand_name VARCHAR2(30) NOT NULL, " \
@@ -28,13 +28,13 @@ brands = "CREATE TABLE brands ( " \
          "rep_first_name VARCHAR2(30) NOT NULL, " \
          "rep_last_name VARCHAR2(30) NOT NULL, " \
          "rep_phone_no VARCHAR2(30) NOT NULL " \
-         ");"
-brands_pk = "CREATE SEQUENCE brand_pk START WITH 1;"
-status = "CREATE TABLE statuses ( " \
+         ")"
+brand_pk = "CREATE SEQUENCE brand_pk START WITH 1;"
+statuses = "CREATE TABLE statuses ( " \
          "status_id INTEGER PRIMARY KEY, " \
          "status_name VARCHAR2(30) NOT NULL" \
-         ");"
-status_pk = "CREATE SEQUENCE status_pk START WITH 1;"
+         ")"
+statuses_pk = "CREATE SEQUENCE status_pk START WITH 1"
 customers = "CREATE TABLE customers ( " \
             "customer_id INTEGER PRIMARY KEY, " \
             "company_name VARCHAR2(30) NOT NULL, " \
@@ -44,13 +44,13 @@ customers = "CREATE TABLE customers ( " \
             "customer_email VARCHAR2(50), " \
             "customer_address VARCHAR(100) NOT NULL, " \
             "customer_city VARCHAR2(30) NOT NULL " \
-            ");"
-customers_pk = "CREATE SEQUENCE c_id START WITH 1;"
-size_pk = "CREATE SEQUENCE size_pk START WITH 1;"
+            ")"
+customers_pk = "CREATE SEQUENCE c_id START WITH 1"
+sizes_pk = "CREATE SEQUENCE size_pk START WITH 1"
 sizes = "CREATE TABLE sizes ( " \
         "size_id INTEGER PRIMARY KEY, " \
         "size_name VARCHAR2(30) NOT NULL " \
-        ");"
+        ")"
 products = "CREATE TABLE products ( " \
            "product_id INTEGER PRIMARY KEY, " \
            "flavour_id INTEGER NOT NULL, " \
@@ -61,25 +61,25 @@ products = "CREATE TABLE products ( " \
            "product_name VARCHAR2(30) NOT NULL," \
            "FOREIGN KEY (flavour_id) REFERENCES flavours(flavour_id)," \
            "FOREIGN KEY (size_id) REFERENCES sizes(size_id)" \
-           ");"
-products_pk = "CREATE SEQUENCE prod_pk START WITH 1;"
-flavouring = "CREATE TABLE flavour_ings ( " \
+           ")"
+products_pk = "CREATE SEQUENCE prod_pk START WITH 1"
+flavour_ing = "CREATE TABLE flavour_ings ( " \
              "flavour_ing_id INTEGER PRIMARY KEY, " \
              "flavour_id INTEGER NOT NULL, " \
              "ingredient_id INTEGER NOT NULL, " \
              "FOREIGN KEY (flavour_id) REFERENCES flavours(flavour_id), " \
              "FOREIGN KEY (ingredient_id) REFERENCES ingredients(ingredient_id) " \
-             ");"
-flavouring_pk = "CREATE SEQUENCE fl_ig_pk START WITH 1;"
+             ")"
+flavour_ing_pk = "CREATE SEQUENCE fl_ig_pk START WITH 1"
 favourites = "CREATE TABLE favourites (" \
              "favourite_id INTEGER PRIMARY KEY, " \
              "product_id INTEGER NOT NULL, " \
              "frequency VARCHAR2(30), " \
              "total_amount INTEGER, " \
              "FOREIGN KEY (product_id) REFERENCES products(product_id) " \
-             ");"
+             ")"
 
-favourites_pk = "CREATE SEQUENCE favourite_id START WITH 1;"
+favourites_pk = "CREATE SEQUENCE favourite_id START WITH 1"
 orders = "CREATE TABLE orders ( " \
          "order_id INTEGER PRIMARY KEY, " \
          "customer_id INTEGER NOT NULL, " \
@@ -91,19 +91,19 @@ orders = "CREATE TABLE orders ( " \
          "status_id INTEGER NOT NULL, " \
          "FOREIGN KEY (customer_id) REFERENCES customers(customer_id)," \
          "FOREIGN KEY (status_id) REFERENCES statuses(status_id) " \
-         ");"
+         ")"
 order_pk = "CREATE SEQUENCE order_id START WITH 1;"
-ord_prod = "CREATE TABLE order_products ( " \
+order_prod = "CREATE TABLE order_products ( " \
            "order_prod_id INTEGER PRIMARY KEY, " \
            "order_id INTEGER NOT NULL, " \
            "product_id INTEGER NOT NULL, " \
            "FOREIGN KEY (order_id) REFERENCES orders(order_id), " \
            "FOREIGN KEY (product_id) REFERENCES products(product_id) " \
-           ");"
-ord_prod_pk = "CREATE SEQUENCE op_id START WITH 1;"
+           ")"
+order_prod_pk = "CREATE SEQUENCE op_id START WITH 1"
 
-create_tables = [flavours, ingredients, brands, suppliers, customers, status, sizes, products, flavouring, favourites,
-                 orders, ord_prod]
-pk_list = [flavour_pk, ingredients_pk, brands_pk, supplier_pk, customers_pk, status_pk, size_pk, products_pk,
-           flavouring_pk, favourites_pk, order_pk, ord_prod_pk]
+create_tables = [flavours, ingredients, brands, suppliers, customers, statuses, sizes, products, flavour_ing, favourites,
+                 orders, order_prod]
+pk_list = [flavour_pk, ingredients_pk, brand_pk, supplier_pk, customers_pk, statuses_pk, sizes_pk, products_pk,
+           flavour_ing_pk, favourites_pk, order_pk, order_prod_pk]
 
