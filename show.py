e5a6c83efdc8f0fcf555a5a13b8fdef38effc10f
Rylan <36867509+rfabico@users.noreply.github.com>
2020-11-30 12:36:29 -0500

    Add files via upload

---
+++ show.py
@@ -0,0 +1,19 @@
+flavours = 'SELECT * FROM flavours'
+ingredients = 'SELECT * FROM ingredients'
+brands = 'SELECT * FROM brands'
+suppliers = 'SELECT * FROM suppliers'
+customers = 'SELECT * FROM customers'
+statuses = 'SELECT * FROM statuses'
+sizes = 'SELECT * FROM sizes'
+products = 'SELECT * FROM products'
+flavour_ing = 'SELECT * FROM flavour_ings'
+favourites = 'SELECT * FROM favourites'
+orders = 'SELECT * FROM orders'
+order_prod = 'SELECT * FROM order_products'
+
+
+
+show_tables = [flavours, ingredients, brands, suppliers, customers, statuses, sizes, products, flavour_ing, favourites,
+                 orders, order_prod]
+tables = ['flavours', 'ingredients', 'brands', 'suppliers', 'customers', 'statuses', 'sizes', 'products', 'flavour_ing',
+          'favourites', 'orders', 'order_prod']
